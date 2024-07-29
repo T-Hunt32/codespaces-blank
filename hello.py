9eeaee7e27100bd907728c2820fce2e68c41fb16
Thomas Hunt <thun366@wgu.edu>
2024-07-29 15:08:59 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,52 @@
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+def hello(to="world"):
+    print("hello,", to)
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
